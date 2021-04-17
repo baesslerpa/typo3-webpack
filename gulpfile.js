b4d31fca34c47f7705ca169ed8ba0715e7997658
Patrick <baesslerpa@gmail.com>
2021-04-17 11:54:40 +0200

    browsersync + basic webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-undef */
 const gulp = require('gulp')
 const browserSync = require('browser-sync').create()
+const webpack = require('webpack-stream')
 const sass = require('gulp-sass')
+// eslint-disable-next-line no-unused-vars
 const debug = require('gulp-debug')
-
 
 const extname = 'test'
 const devDomain = 'local.webpack.net'
@@ -23,24 +24,38 @@ const paths = {
 	}
 }
 
+
 // Static Server + watching scss/html files
 function serve() {
 	browserSync.init({
 		proxy: devDomain,
 	})
+	gulp.watch(paths.scripts.src, webpackWatch)
 	gulp.watch(paths.styles.entry, sassCompile)
 	gulp.watch(paths.templates.entry).on('change', browserSync.reload)
 }
 
-// Compile sass into CSS & auto-inject into browsers
+function webpackWatch() {
+	return gulp.src(paths.scripts.src)
+		.pipe(webpack({
+			output: {
+				filename: '[name].bundled.js',
+			}
+		}))
+		.pipe(gulp.dest(paths.scripts.dest))
+		.pipe(browserSync.stream())
+}
 
+// Compile sass into CSS & auto-inject into browsers
 function sassCompile() {
+	console.log('sass')
 	return gulp.src(paths.styles.entry)
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest(paths.styles.output))
 		.pipe(browserSync.stream())
 }
 
-exports.serve = serve
-exports.sass = sassCompile
 
+exports.sass = sassCompile
+exports.webpack = webpackWatch
+exports.serve = serve
